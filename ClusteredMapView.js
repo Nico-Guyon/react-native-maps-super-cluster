1c45174b9da08ce8bf84e22f15a30e94bacf5e19
Nicolas Guyon <shinkschn@gmail.com>
2021-10-04 15:16:26 +0200

    added UNSAFE for warning removal from componentXXXX

--- ClusteredMapView.js
+++ ClusteredMapView.js
@@ -39,16 +39,16 @@ export default class ClusteredMapView extends PureComponent {
     this.onRegionChangeComplete = this.onRegionChangeComplete.bind(this)
   }
 
-  componentDidMount() {
+  UNSAFE_componentDidMount() {
     this.clusterize(this.props.data)
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if (this.props.data !== nextProps.data)
       this.clusterize(nextProps.data)
   }
 
-  componentWillUpdate(nextProps, nextState) {
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
     if (!this.isAndroid && this.props.animateClusters && this.clustersChanged(nextState))
       LayoutAnimation.configureNext(this.props.layoutAnimationConf)
   }
